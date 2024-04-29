c929f939a3d964e6dc2d2f515e9a5e732f39d9c8
Ferdziu10 <ferdziu103@gmail.com>
2024-04-29 12:23:47 +0200

    wersja ostateczna

--- rtl/draw_rct.sv
+++ rtl/draw_rct.sv
@@ -3,14 +3,20 @@
 module draw_rct (
     vga_if.in vii, 
     vga_if.out vio,
+    input  logic [11:0] xpos,
+    input  logic [11:0] ypos,
     input  logic clk,
-    input  logic rst
+    input  logic rst, 
+    input  logic [11:0] rgb_pixel,
+    output logic [11:0] pixel_addr
+
     );
     import vga_pkg::*;
  /* Local variables and signals
  */
 
  vga_if vga_nxt ();
+ logic [11:0] pixel_addr_nxt;
 
 always_ff @(posedge clk) begin : bg_ff_blk
     if (rst) begin
@@ -21,6 +27,8 @@ always_ff @(posedge clk) begin : bg_ff_blk
         vio.hsync  <= '0;
         vio.hblnk  <= '0;
         vio.rgb    <= '0;
+        pixel_addr <= '0;
+
     end else begin
         vio.vcount <= vii.vcount;
         vio.vsync  <= vii.vsync;
@@ -29,13 +37,16 @@ always_ff @(posedge clk) begin : bg_ff_blk
         vio.hsync  <= vii.hsync;
         vio.hblnk  <= vii.hblnk;
         vio.rgb    <= vga_nxt.rgb;
+        pixel_addr <= pixel_addr_nxt;
     end
 end
 
 
 always_comb begin
-    if ( vii.hcount >= X_RCTSTR && vii.hcount <= X_RCTSTR + WID_RCT && vii.vcount >= Y_RCTSTR && vii.vcount <= Y_RCTSTR + HGH_RCT )
-    vga_nxt.rgb = COL_RCT;
+    pixel_addr_nxt = {6'(vii.vcount - ypos), 6'(vii.hcount - xpos)};
+    if ( vii.hcount >= xpos + 2 && vii.hcount <= xpos + WID_RCT + 2  && vii.vcount >= ypos && vii.vcount <= ypos + HGH_RCT ) begin
+    vga_nxt.rgb = rgb_pixel;
+
 /*
     else if ( vcount_in >= X_RCTSTR && vcount_in <= X_RCTSTR + WID_RCT && hcount_in == Y_RCTSTR + HGH_RCT )
         rgb_nxt = COL_RCT;
@@ -44,7 +55,8 @@ always_comb begin
     else if ( hcount_in >= Y_RCTSTR && hcount_in <= Y_RCTSTR + HGH_RCT && vcount_in == X_RCTSTR )
         rgb_nxt = COL_RCT;
 */
-    else 
+    end else begin 
     vga_nxt.rgb = vii.rgb;
+    end
 end
 endmodule
